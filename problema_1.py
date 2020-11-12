618b6f470535457fa27acc8ece4b953379063604
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2020-11-12 11:43:57 +0300

    Add files via upload

---
+++ problema_1.py
@@ -0,0 +1,11 @@
+n=int(input("dati numarul natural n: "))
+if(n==28):
+    print("februarie")
+elif(n==29):
+    print("februarie odata la 4 ani")
+elif(n==30):
+    print("aprilie,iunie,septembrie,noiembrie")
+elif(n==31):
+    print("ianuarie,martie,mai,iulie,august,octombrie,decembrie")
+else:
+    print("nu exista luna cu asa numar de zile")
