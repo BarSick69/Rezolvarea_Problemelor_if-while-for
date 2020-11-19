7cca18923490b6a776f2f6896e4a7cd1ed3bf1ea
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2020-11-19 18:12:58 +0200

    Update problema_4.py

--- problema_4.py
+++ problema_4.py
@@ -1,27 +1,10 @@
 from fractions import Fraction
-import math
-numaratorul_1=int(input("dati numaratorul 1: "))
-numar1=numaratorul_1
-numitorul_1=int(input("dati numitorul 1: "))
-numaratorul_2=int(input("dati numaratorul 2: "))
-numar2=numaratorul_2
-numitorul_2=int(input("dati numitorul 2: "))
-if(numitorul_1!=numitorul_2):
-    l=numitorul_2
-    n=numitorul_1
-    numitorul_1=n*l
-    numitorul_2=l*n
-    numaratorul_1=l*numaratorul_1
-    numaratorul_2=n*numaratorul_2
-l=numitorul_2
-n=numitorul_1
-suma=numaratorul_1+numaratorul_2
-s1mple=math.gcd(suma,numitorul_2)
-suma=suma//s1mple
-numitorul_2=numitorul_2//s1mple
-print("Suma: ",Fraction(suma,numitorul_2))
-print("Produsul: ",Fraction(numar1*numar2,n*l))
-   
-
-
-
+m = int(input("dati numaratorul1: "))
+n = int(input("dati numitorul1: "))
+a = int(input("dati numaratorul2: "))
+b = int(input("dati numitorul2: "))
+if ((n!=0) and (b!=0)):
+    print(Fraction(a,b)+Fraction(m,n))
+    print(Fraction(a,b)*Fraction(m,n))
+else:
+    print("Numitorul nu poate fi 0")
